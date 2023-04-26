30a12e3bd9ba9ac98c7682ac59361e1b36ac1e88
syncgui <guilhermeyla@gmail.com>
2023-04-26 16:50:11 -0300

    new java exemples using lambdas functions with threads

--- java/src/main/java/github/syncgui/lambdas/Desafio.java
+++ java/src/main/java/github/syncgui/lambdas/Desafio.java
@@ -1,5 +1,6 @@
 package github.syncgui.lambdas;
 
+import java.util.Locale;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
@@ -7,13 +8,15 @@ public class Desafio {
 
     public static void main(String[] args) {
 
+        System.setProperty("user.language.format","en");
+
         Function<Produto, Double> precoFinal = preco -> preco.preco * (1 - preco.desconto);
 
         UnaryOperator<Double> aplicarImposto = preco -> preco >= 2500 ? preco * 1.085 : preco;
 
         UnaryOperator<Double> frete = preco -> preco >= 3000 ? preco + 100 : preco + 50;
 
-        UnaryOperator<Double> arredondar = preco -> Double.parseDouble(String.format("%.2f", preco));
+        UnaryOperator<Double> arredondar = preco -> Double.parseDouble(String.format(Locale.ENGLISH, "%.2f", preco));
 
         Function<Double, String> formatar = preco -> ("R$" + preco).replace(".", ",");
 
