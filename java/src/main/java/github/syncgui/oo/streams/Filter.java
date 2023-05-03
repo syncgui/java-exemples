f178791561f8466acac283e2050b4e3de1e172bb
syncgui <guilhermeyla@gmail.com>
2023-05-03 19:29:55 -0300

    new java exemples using lambdas functions with threads

---
+++ java/src/main/java/github/syncgui/oo/streams/Filter.java
@@ -0,0 +1,32 @@
+package github.syncgui.oo.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Filter {
+
+    public static void main(String[] args) {
+
+        Aluno a1 = new Aluno("Ana", 7.8);
+        Aluno a2 = new Aluno("Bia", 5.8);
+        Aluno a3 = new Aluno("Daniel", 9.8);
+        Aluno a4 = new Aluno("Gui", 6.8);
+        Aluno a5 = new Aluno("Rebeca", 7.1);
+        Aluno a6 = new Aluno("Pedro", 8.8);
+
+        List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6);
+
+        Predicate<Aluno> isApproved = n -> n.nota >= 7.0;
+        Function<Aluno, String> formatApproved = n -> "Parabéns " + n.nome + ", você foi aprovado(a) com " + n.nota;
+
+        alunos.stream()
+                .filter(isApproved)
+                .map(formatApproved)
+                .forEach(System.out::println);
+
+
+
+    }
+}
