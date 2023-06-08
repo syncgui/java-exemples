74e6d17f0621c7662ca88ebb76d2f3e62c8dfb8f
syncgui <guilhermeyla@gmail.com>
2023-06-08 16:00:02 -0300

    new exemples using generics

---
+++ java/src/main/java/github/syncgui/generics/Par.java
@@ -0,0 +1,46 @@
+package github.syncgui.generics;
+
+import java.util.Objects;
+
+public class Par<C, V> {
+
+    private C chave;
+    private V valor;
+
+    public Par() {
+    }
+
+    public Par(C chave, V valor) {
+        this.chave = chave;
+        this.valor = valor;
+    }
+
+    public C getChave() {
+        return chave;
+    }
+
+    public V getValor() {
+        return valor;
+    }
+
+    public void setChave(C chave) {
+        this.chave = chave;
+    }
+
+    public void setValor(V valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Par<?, ?> par = (Par<?, ?>) o;
+        return Objects.equals(chave, par.chave) && Objects.equals(valor, par.valor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chave, valor);
+    }
+}
